b8c8f167dd8467362c0c8767f9811147eefb20b2
Your Name <you@example.com>
2018-05-29 21:37:26 -0300

    fix controller user

--- life_check/app/controllers/api/v1/users_controller.rb
+++ life_check/app/controllers/api/v1/users_controller.rb
@@ -49,7 +49,7 @@
 
       def sign_up_params
         require_parameters([:email, :password,:name,:contact,:age,:weigth,:heigth])
-        params.permit(:email, :password)
+        params.permit(:email, :password,:name,:contact,:age,:weigth,:heigth)
       end
 
       def update_params
